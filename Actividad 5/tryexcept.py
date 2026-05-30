42ad101aa3addbff8558cc11f3506a3d7c098633
Saamxs <samuelquirozrinaldii@gmail.com>
2026-05-30 13:35:49 -0500

    Gestor De Finanzas de Quirozzz

--- Actividad 5/tryexcept.py
+++
@@ -1,5 +0,0 @@
-try:
-    # Riesgo de error de tipo o valor
-    res = 10 / int("a")
-except (ZeroDivisionError, ValueError) as e:
-    print(f"Ocurrió un error: {e}")
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Evaluacion-repo
+# ProyectoFinanzas
+# ProyectoFinanzas
